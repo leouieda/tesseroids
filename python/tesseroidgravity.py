42bb7b3cd49b7fe8c8f761080d355f80240d6c8e
leouieda <none@none>
2009-04-07 05:29:04 +0000

    Finished the TesseroidPotential. Still testing and running some TimeIts.
    --HG--
    extra : convert_revision : svn%3A0beaa508-17e7-11de-9433-ad2e3fad96c5/trunk%4019

--- python/tesseroidgravity.py
+++ python/tesseroidgravity.py
@@ -42,6 +42,7 @@ TesseroidGravity:
          Leonardo Uieda - leouieda@gmail.com
 
 """
+import time
 ################################################################################
 __author__ = '$Author$'
 # Created: 6-Apr-2009 4:07:57 PM
@@ -50,8 +51,7 @@ __date__ = 'Last edited: $Date$'
 ################################################################################
 
 
-import glq
-from math import pi, cos, sin
+from math import pi, cos, sin, sqrt
 
 
 ################################################################################
@@ -93,12 +93,17 @@ class TesseroidGravityError(Exception):
 
 
 ################################################################################
-# TESSEROIDPOTENTIAL CLASS
+# TESSEROIDGRAVITY CLASS
 class TesseroidGravity:
     """
     Mother class to the Tesseroid* classes (* are the diff. fields).
     This class has the constants shared by it's children.
     If these need be changed, change them here.
+    Constants are:
+        * G = Gravitational constant in m**3 kg**(-1) s**(-1)
+        * R = Mean Earth radius in m
+        * si2eotvos = Conversion factor from SI units to Eotvos
+        * si2mgal = Conversion factor from SI units to mGal
     """
     # The gravitational constant (m^3*kg^-1*s^-1)
     G = 0.00000000006673
@@ -108,23 +113,17 @@ class TesseroidGravity:
     si2eotvos = 1000000000
     # Conversion factor from SI units to mGal: 1 m/s**2 = 10**5 mGal
     si2mgal = 100000
-  
-
-################################################################################
-
-
-################################################################################
-# TESSEROIDPOTENTIAL CLASS
-class TesseroidPotential:
-    """
-    Calculates and stores the gravity potential of a tesseroid model.
-
-    """
 
     def __init__(self, abr, ablon, ablat, wr, wlon, wlat):
         """
         Pass the Abscissas and associated Weights that will be used in the GLQ.
-        """        
+            abr   - Abscissas in the radial direction;
+            ablon - Abscissas in the longitude direction;
+            ablat - Abscissas in the latitude direction;
+            wr    - Weights in the radial direction;
+            wlon  - Weights in the longitude direction;
+            wlat  - Weights in the latitude direction;
+        """
         # Initialize the parameters
         self.abr = abr
         self.ablon = ablon
@@ -133,11 +132,49 @@ class TesseroidPotential:
         self.wlon = wlon
         self.wlat = wlat
 
+
+    def calculate(self, model, grid):
+        """
+        Method implemented in the derived classes to calculate the gravity field
+        of 'model' in the points of 'grid'
+            model - should be a 1D list of Tesseroids.
+            grid  - 2D list with grid[:][0] having the lon values, grid[:][1]
+                    the lat values, and grid[:][2] the height values.
+                    Lon and lat should be in degrees and height in meters.
+        Returns a list with the field values on the respective grid points.
+        """
+        pass
+
+
+################################################################################
+
+
+################################################################################
+# TESSEROIDPOTENTIAL CLASS
+class TesseroidPotential(TesseroidGravity):
+    """
+    Calculates and stores the gravity potential of a tesseroid model.
+
+    """
+
+    def __init__(self, abr, ablon, ablat, wr, wlon, wlat):
+        """
+        Pass the Abscissas and associated Weights that will be used in the GLQ.
+            abr   - Abscissas in the radial direction;
+            ablon - Abscissas in the longitude direction;
+            ablat - Abscissas in the latitude direction;
+            wr    - Weights in the radial direction;
+            wlon  - Weights in the longitude direction;
+            wlat  - Weights in the latitude direction;
+        """
+        # Call the base class constructor to set the parameters
+        TesseroidGravity.__init__(self, abr, ablon, ablat, wr, wlon, wlat)
+
         # Create a Logger for the TesseroidPotential class
         self.log = logging.getLogger('tesseroid.TesseroidPotential')
 
 
-    def calculate(self, model, grid):
+    def calculate(self, model, lons, lats, heights):
         """
         Calculate the gravity potential field of 'model' in the points of 'grid'
             model - should be a 1D list of Tesseroids.
@@ -150,13 +187,70 @@ class TesseroidPotential:
         # Conversion factor from degrees to radians.
         deg2rad = pi/180.0
 
+        # First initialize the array that will hold the field values
+        field = []
+
         # Iterate over the tesseroids in the model and sum their fields on the
         # grid points.
         for tess in model:
 
-            self.abr.scale()
-        
-        return ()
+            # Scale the abscissas to the dimensions of the tesseroid
+            self.abr.scale(self.R-tess['bottom'], self.R-tess['top'])
+            self.ablon.scale(deg2rad*tess['w'], deg2rad*tess['e'])
+            self.ablat.scale(deg2rad*tess['s'], deg2rad*tess['n'])
+
+            # This is the scale fator that will multiply the GLQ summation due
+            # to the scaling of the abscissas
+            # For practicality I included the density of the tesseroid and the
+            # gravitational constants as well.
+            factor = ((deg2rad**2)*(tess['e']-tess['w'])*(tess['n']-tess['s'])*\
+                     (tess['bottom']-tess['top'])*tess['density']*self.G) / 8.0
+
+            # Caculate the field at every point on the grid and sum to what was
+            # there before.
+            for i, phi in zip(*[range(0, len(lats)), lats]):
+
+                # Start a new line in field
+                field.append([])
+
+                # Convert the latitude to radians
+                phi *= deg2rad
+                
+                for lamb, h in zip(*[lons, heights[i]]):
+                    
+                    # Calculate the radial distance to the point
+                    r = self.R + h
+
+                    # Convert the longitude to radians
+                    lamb *= deg2rad
+                    
+                    # Initialize the result with zero
+                    res = 0.0
+
+                    # Do the GLQ summation to calculate the field
+                    for lambl, wlon in zip(*[self.ablon.val, self.wlon.val]):
+                        for phil, wlat in zip(*[self.ablat.val, self.wlat.val]):
+                            for rl, wr in zip(*[self.abr.val, self.wr.val]):
+
+                                # Calculate the integral kernel K
+                                rl_2 = rl**2
+                                cosPhil = cos(phil)
+                                cosPsi = sin(phi)*sin(phil) + cos(phi)*cosPhil*\
+                                                                    cos(lamb-lambl)
+                                l = sqrt( (r*r) + (rl_2) - (2*r*rl*cosPsi) )
+                                K = (rl_2*cosPhil) / l
+
+                                # Add the kernel at the current integration point
+                                # to the result
+                                res += wlon*wlat*wr*K
+
+                    # Now that it's all added, multiply the scale factor
+                    res *= factor
+
+                    # Add this to the field list
+                    field[i].append(res)
+
+        return field
 
 
 #    def check_abscissas(self):
@@ -167,3 +261,42 @@ class TesseroidPotential:
 #        pass
 
 ################################################################################
+
+
+if __name__ == '__main__':
+
+    from timeit import Timer
+    
+    str = "import glq\n" + \
+        "import pylab as p\n" + \
+        "import scipy as s\n" + \
+        "import tesseroid as t\n" + \
+        "from __main__ import TesseroidPotential\n" + \
+        "tess = t.Tesseroid(27.5,32.5,-2.5,2.5,0,40000,1.0,'TEST')\n" + \
+        "ablon = glq.Abscissas(5)\n" + \
+        "ablat = glq.Abscissas(5)\n" + \
+        "abr = glq.Abscissas(5)\n" + \
+        "wlon = glq.Weights(ablon)\n" + \
+        "wlat = glq.Weights(ablat)\n" + \
+        "wr = glq.Weights(abr)\n" + \
+        "lon = s.arange(0,60.1,0.1)\n" + \
+        "lat = s.arange(-30,30.1,0.1)\n" + \
+        "h = 100000*p.ones((len(lat), len(lon)))\n" + \
+        "tp = TesseroidPotential(abr,ablon,ablat,wr,wlon,wlat)\n"
+    
+    print "Timing calculate..."
+
+    time = Timer(stmt='tp.calculate([tess], lon, lat, h)', setup=str)
+    res = time.repeat(20)
+    print "min: ", min(res), " max: ", max(res)
+    print "times: ", res
+    #pot = tp.calculate([tess], lon, lat, h)
+    #lons, lats = p.meshgrid(lon,lat)
+#
+#    p.pcolor(lons, lats, pot, cmap=p.cm.jet)
+#    p.colorbar(orientation='horizontal',aspect=50,shrink=0.5)
+#    p.show()
+
+    
+
+
