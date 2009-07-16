9074e0c90c565915f87f7b21081f688890ba95cc
leouieda <none@none>
2009-07-16 04:48:03 +0000

    Added a txt with the glp3.0 Made an Inno Setup script that also sets the curent users path and asks for a restart Changed the setup to include py2exe
    --HG--
    extra : convert_revision : svn%3A0beaa508-17e7-11de-9433-ad2e3fad96c5/trunk%4046

--- python/setup.py
+++ python/setup.py
@@ -37,19 +37,51 @@ The setup script for the Tesseroids software package.
 
 from distutils.core import setup
 
-setup( \
-  name='tesseroids', \
-  fullname='Tesseroids', \
-  description="Direct modeling of gravity fields in spherical coordinates", \
-  version='0.3', \
-  author="Leonardo Uieda", \
-  author_email='leouieda@gmail.com', \
-  license='GPL', \
-  url='http://code.google.com/p/tesseroids/', \
-  scripts=['tesspot', \
+name = 'tesseroids'
+fullname = 'Tesseroids'
+description = "Direct modeling of gravity fields in spherical coordinates"
+version = '0.3'
+author = "Leonardo Uieda"
+author_email = 'leouieda@gmail.com'
+license = 'GPL'
+url = 'http://code.google.com/p/tesseroids/'
+scripts = ['tesspot', \
            'tessgx', 'tessgy', 'tessgz', \
            'tessgxx', 'tessgxy', 'tessgxz', \
                       'tessgyy', 'tessgyz', \
-                                 'tessgzz'], \
-  py_modules=['glq'], \
-  packages=['tesseroids_lib'])
+                                 'tessgzz']
+py_modules = ['glq']
+packages = ['tesseroids_lib']
+
+try:
+    # If the windows system has py2exe installed, import it and make
+    # the setup as it should be
+    import py2exe
+
+    setup(name=name, \
+          fullname=fullname, \
+          description=description, \
+          version=version, \
+          author=author, \
+          author_email=author_email, \
+          license=license, \
+          url=url, \
+          console=scripts, \
+          py_modules=py_modules, \
+          packages=packages)
+          
+except ImportError:
+    # If it doesn't have py2exe or it's not windows, just make it
+    # a normal source setup
+    setup(name=name, \
+          fullname=fullname, \
+          description=description, \
+          version=version, \
+          author=author, \
+          author_email=author_email, \
+          license=license, \
+          url=url, \
+          scripts=scripts, \
+          py_modules=py_modules, \
+          packages=packages)
+      
