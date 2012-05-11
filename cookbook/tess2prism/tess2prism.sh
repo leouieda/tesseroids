ebe6e4f9988c56027edeae95eafbd355a1136312
Leonardo Uieda <leouieda@gmail.com>
2012-05-11 16:41:44 -0300

    Added recipe for prism calculation in spherical coordinates using tess2prism

---
+++ cookbook/tess2prism/tess2prism.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Generate a prism model from a tesseroid model.
+# Prisms will have the same mass as the tesseroids and
+# associated spherical coordinates of the center of
+# the top of the tesseroid.
+
+tess2prism < tess-model.txt > prism-model.txt
+
+# Generate a regular grid in spherical coordinates,
+# pipe the grid to the computation programs,
+# and dump the result on output.txt
+# prismpots calculates the potential in spherical
+# coordinates, prismgs calculates the full
+# gravity vector, and prismggts calculates the full
+# gravity gradient tensor.
+
+tessgrd -r-30/30/-30/30 -b50/50 -z250e03 | \
+prismpots prism-model.txt | \
+prismgs prism-model.txt | \
+prismggts prism-model.txt > output.txt
