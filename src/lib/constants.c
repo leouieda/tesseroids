8bd7f2eac048c1aa1709ad0857870de1fb906955
Leonardo Uieda <leouieda@gmail.com>
2015-02-28 13:44:03 -0300

    Update division ratios to latest results of paper

--- src/lib/constants.c
+++ src/lib/constants.c
@@ -26,14 +26,14 @@ const double PI = 3.1415926535897932384626433832795;
 /* minimum distance-to-size ratio for potential computations to be accurate */
 const double TESSEROID_POT_SIZE_RATIO = 1;
 /* Minimum distance-to-size ratio for gravity computations to be accurate */
-const double TESSEROID_GX_SIZE_RATIO = 1;
-const double TESSEROID_GY_SIZE_RATIO = 1;
-const double TESSEROID_GZ_SIZE_RATIO = 1;
+const double TESSEROID_GX_SIZE_RATIO = 1.5;
+const double TESSEROID_GY_SIZE_RATIO = 1.5;
+const double TESSEROID_GZ_SIZE_RATIO = 1.5;
 /* Minimum distance-to-size ratio for gravity gradient computations to be
 accurate */
-const double TESSEROID_GXX_SIZE_RATIO = 6;
-const double TESSEROID_GXY_SIZE_RATIO = 6;
-const double TESSEROID_GXZ_SIZE_RATIO = 6;
-const double TESSEROID_GYY_SIZE_RATIO = 6;
-const double TESSEROID_GYZ_SIZE_RATIO = 6;
-const double TESSEROID_GZZ_SIZE_RATIO = 6;
+const double TESSEROID_GXX_SIZE_RATIO = 8;
+const double TESSEROID_GXY_SIZE_RATIO = 8;
+const double TESSEROID_GXZ_SIZE_RATIO = 8;
+const double TESSEROID_GYY_SIZE_RATIO = 8;
+const double TESSEROID_GYZ_SIZE_RATIO = 8;
+const double TESSEROID_GZZ_SIZE_RATIO = 8;
